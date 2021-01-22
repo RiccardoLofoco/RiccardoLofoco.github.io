379210a129b61606e26a51b8308351eb116b3772
RiccardoLofoco <77680937+RiccardoLofoco@users.noreply.github.com>
2021-01-22 15:47:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 
 //quando si clicca su un'immagine, viene aperta a tutto schermo
 
-document.getElementsByTagName("img").addEventListener(click, function(){
+document.getElementsByTagName("img").addEventListener("click", function(){
   window.open(this.src);
 })
