66daa876f45d5bc2c8883247c9e61fabb9f64411
RiccardoLofoco <77680937+RiccardoLofoco@users.noreply.github.com>
2021-01-22 16:06:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 //quando si clicca su un'immagine, viene aperta a tutto schermo
 
-immagini[] = document.getElementsByTagName("img");
+immagini = document.getElementsByTagName("img");
 
 for (immagine in immagini){
   immagine.addEventListener("contextmenu", function(){if(confirm("Aprire l'immagine?")){( window.open(immagine.url);}));
