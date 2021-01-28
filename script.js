774e84517625b02ef5a3a759823a9b85c638a29d
RiccardoLofoco <77680937+RiccardoLofoco@users.noreply.github.com>
2021-01-29 00:04:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //script per il sito
 
 //quando si clicca su un'immagine, viene aperta a tutto schermo
-
-immagini = document.getElementsByTagName("img");
 /*
+immagini = document.getElementsByTagName("img");
+
 function apriimg(imgn){
-  /*if(confirm("Aprire l'immagine?")){
+  if(confirm("Aprire l'immagine?")){
     window.open(imgn.src);
-  }*/
+  }
 }
 console.log(immagini);
 
